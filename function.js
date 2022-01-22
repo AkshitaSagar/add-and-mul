2b5ace402fe7d285e96042550c31e4a33f2879af
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2022-01-22 22:24:26 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+//multiplication
+ a = [2, 5, 8, 9]
+var result = []
+for(let i = 0; i<a.length; i++){
+    console.log(a);
+   
+    var b =(a[i]*4);
+    result.push(b)
+    console.log('The multiplication of given array is' + b);
+}
+//addition
+var add = [2, 5, 8, 9]
+var result = []
+for(let i = 0; i<a.length; i++){
+    console.log(a);
+   
+    var b =(add[i]+9);
+    result.push(b)
+    console.log('The addition of given array is' + b);
+}
+
